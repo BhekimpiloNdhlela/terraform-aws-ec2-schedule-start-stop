51760c6d687909775a3e1fed373d20536867166e
Bheki Ndhlela <bhekhindhlela@wfs.coza>
2025-03-05 12:06:29 +0200

    complete module

--- modules/terraform-aws-automate-ec2-start-stop/main.tf
+++ modules/terraform-aws-automate-ec2-start-stop/main.tf
@@ -1,64 +1,13 @@
-data "aws_caller_identity" "current" {}
-
 # IAM role for Lambda function
 resource "aws_iam_role" "lambda_execution_role" {
   name               = "${var.naming_prefix}-lambda-execution-role"
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
 }
 
-# IAM policy for Lambda to allow necessary actions
-resource "aws_iam_policy" "lambda_execution_policy" {
-  name        = "${var.naming_prefix}-lambda-execution-policy"
-  description = "IAM policy for Lambda function to manage EC2, SNS, and CloudWatch Logs"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "sns:Publish"
-        ]
-        Resource = aws_sns_topic.automate_ec2_start_stop.arn
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "ec2:StartInstances",
-          "ec2:StopInstances",
-          "ec2:DescribeInstances"
-        ]
-        Resource = "*"
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ]
-        Resource = "arn:aws:logs:*:*:*"
-      }
-    ]
-  })
-}
-
 # Attach the policy to the IAM role
 resource "aws_iam_role_policy_attachment" "lambda_execution_policy_attachment" {
   role       = aws_iam_role.lambda_execution_role.name
   policy_arn = aws_iam_policy.lambda_execution_policy.arn
-}
-
-# Data for Lambda assume role policy
-data "aws_iam_policy_document" "lambda_assume_role_policy" {
-  statement {
-    effect = "Allow"
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-    actions = ["sts:AssumeRole"]
-  }
 }
 
 # Reference the Lambda role in the Lambda function resource
@@ -116,13 +65,6 @@ resource "aws_lambda_permission" "eventbridge_invoke" {
   source_arn    = aws_cloudwatch_event_rule.ec2_schedule[each.key].arn
 }
 
-# Archive the Lambda function code into a zip file
-data "archive_file" "automate_ec2_start_stop" {
-  type        = "zip"
-  source_dir  = "${path.module}/lambda/automate-ec2-start-stop"
-  output_path = "${path.module}/builds/lambda/automate-ec2-start-stop.zip"
-}
-
 # CloudWatch log group for the Lambda function
 resource "aws_cloudwatch_log_group" "automate_ec2_start_stop" {
   name              = "/aws/lambda/${var.naming_prefix}-logging"
@@ -146,24 +88,6 @@ resource "aws_sns_topic_subscription" "email_subscription" {
   protocol               = "email"
   endpoint_auto_confirms = true
   endpoint               = each.value
-}
-
-# Define a map for EventBridge rules
-locals {
-  eventbridge_rules = {
-    start = {
-      name_suffix     = "start-ec2-instances"
-      description     = "EventBridge rule to trigger Lambda function to start EC2 instances at 6 AM (Monday to Friday)"
-      cron_expression = var.start_expression
-      action          = "start" # Custom flag for action
-    }
-    stop = {
-      name_suffix     = "stop-ec2-instances"
-      description     = "EventBridge rule to trigger Lambda function to stop EC2 instances at 6 PM (Monday to Friday)"
-      cron_expression = var.stop_expression
-      action          = "stop" # Custom flag for action
-    }
-  }
 }
 
 # Create EventBridge rules
