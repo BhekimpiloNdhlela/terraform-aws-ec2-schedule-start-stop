a0db6d745a8025ac143969ae26ac61acf41b7562
Bheki Ndhlela <bhekhindhlela@wfs.coza>
2025-03-05 12:36:43 +0200

    complete module

--- modules/terraform-aws-automate-ec2-start-stop/lambda/automate-ec2-start-stop/index.py
+++ modules/terraform-aws-automate-ec2-start-stop/lambda/automate-ec2-start-stop/index.py
@@ -1,12 +1,14 @@
 import boto3
 import os
 import json
-from typing import List, Optional
+from typing import List, Optional, Dict
 import requests
 
+# AWS Clients
 ec2_client = boto3.client("ec2")
 sns_client = boto3.client("sns")
 
+# environment Variables
 SNS_TOPIC_ARN = os.environ.get("SNS_TOPIC_ARN")
 ERROR_EMAIL_SUBJECT = os.environ.get("ERROR_EMAIL_SUBJECT")
 ERROR_EMAIL_HEADER = os.environ.get("ERROR_EMAIL_HEADER")
@@ -26,8 +28,8 @@ def send_email_notification(subject: str, message: str) -> None:
     Sends a notification via SNS.
 
     Args:
-        subject (str): The subject line for the SNS message.
-        message (str): The body of the SNS message.
+        subject (str): The subject of the email notification.
+        message (str): The body of the email notification.
 
     Returns:
         None
@@ -40,7 +42,8 @@ def send_email_notification(subject: str, message: str) -> None:
         )
         print("[INFO]: SNS notification sent successfully.")
     except Exception as e:
-        raise Exception(f"[ERROR]: Failed to send SNS notification: {e}")
+        print(f"[ERROR]: Failed to send SNS notification: {e}")
+        raise
 
 def send_ms_teams_notification(message: str) -> None:
     """
@@ -64,37 +67,42 @@ def send_ms_teams_notification(message: str) -> None:
         response.raise_for_status()
         print(f"[INFO]: Message sent to Teams successfully. Status Code: {response.status_code}")
     except requests.exceptions.RequestException as e:
-        raise Exception(f"[ERROR]: Failed to send message to Teams: {e}")
+        print(f"[ERROR]: Failed to send message to Teams: {e}")
+        raise
 
 def log_and_report_process_results(error: bool, message: str) -> None:
     """
     Logs the process results and reports them via SNS and Microsoft Teams.
 
     Args:
-        error (bool): Whether the process resulted in an error.
-        message (str): The log message to report.
+        error (bool): Indicates whether the process encountered an error.
+        message (str): The message to be logged and reported.
 
     Returns:
         None
     """
-    print(message)
+    print(f"[ERROR]: {message}" if error else f"[INFO]: {message}")
 
-    if error: formatted_message = f"{ERROR_EMAIL_HEADER}\n{message}\n{ERROR_EMAIL_FOOTER}"
-    else: formatted_message = f"{SUCCESS_EMAIL_HEADER}\n{message}\n{SUCCESS_EMAIL_FOOTER}"
-
+    formatted_message = ""
+    if error:
+        formatted_message = f"{ERROR_EMAIL_HEADER}\n{message}\n{ERROR_EMAIL_FOOTER}" 
+    else:
+        formatted_message = f"{SUCCESS_EMAIL_HEADER}\n{message}\n{SUCCESS_EMAIL_FOOTER}"
     send_ms_teams_notification(formatted_message)
     send_email_notification(ERROR_EMAIL_SUBJECT if error else SUCCESS_EMAIL_SUBJECT, formatted_message)
 
-def get_ec2_instance_ids_by_schedule_tag(tag_key, tag_value):
+def get_ec2_instance_ids_by_schedule_tag(tag_key: str, tag_value: str) -> List[str]:
     """
-    List EC2 instances filtered by a specific tag key and value.
-    
-    :param tag_key: The key of the tag to filter instances by (e.g., 'auto-start').
-    :param tag_value: The value of the tag to filter instances by (e.g., 'true' or 'false').
-    :return: A list of dictionaries containing instance IDs and tags.
+    Retrieves EC2 instances that have a specific tag key and value.
+
+    Args:
+        tag_key (str): The key of the tag used to filter instances.
+        tag_value (str): The value of the tag used to filter instances.
+
+    Returns:
+        List[str]: A list of instance IDs matching the tag key and value.
     """
     instance_ids = []
-
     paginator = ec2_client.get_paginator('describe_instances')
     page_iterator = paginator.paginate()
 
@@ -102,71 +110,81 @@ def get_ec2_instance_ids_by_schedule_tag(tag_key, tag_value):
         for reservation in page['Reservations']:
             for instance in reservation['Instances']:
                 tags = instance.get('Tags', [])
-                
-                # check if the instance has the specified tag with the desired value
                 for tag in tags:
                     if tag['Key'] == tag_key and tag['Value'].lower() == tag_value.lower():
                         instance_ids.append(instance['InstanceId'])
-                        # no need to check further tags for this instance
+                        # no need to check other tags if the instance matches the schedule tag
                         break
     return instance_ids
 
-
-def stop_ec2_instance(ec2_instanceIds) -> None:
+def stop_ec2_instances(instance_ids: List[str]) -> None:
     """
-    Stops an EC2 instance.
+    Stops EC2 instances.
 
     Args:
-        instance_id (str): The ID of the EC2 instance to stop.
-        failed_instances (List[str]): A list to collect IDs of instances that failed to stop.
+        instance_ids (List[str]): A list of EC2 instance IDs to stop.
 
     Returns:
         None
     """
+    if not instance_ids:
+        print("[INFO]: No EC2 instances to stop.")
+        return
     try:
-        ec2_client.stop_instances(InstanceIds=ec2_instanceIds)
-        print(f"[INFO]: Successfully stopped instance(s): '{ec2_instanceIds}'.")
+        ec2_client.stop_instances(InstanceIds=instance_ids)
+        print(f"[INFO]: Successfully stopped instances: {instance_ids}")
     except Exception as e:
-        raise Exception(f"[ERROR]: Failed to start instance {ec2_instanceIds}: {str(e)}")
+        print(f"[ERROR]: Failed to stop instances {instance_ids}: {e}")
+        raise
 
-def start_ec2_instance(ec2_instanceIds) -> None:
+def start_ec2_instances(instance_ids: List[str]) -> None:
     """
-    Starts an EC2 instance.
+    Starts EC2 instances.
 
     Args:
-        instance_id (str): The ID of the EC2 instance to start.
-        failed_instances (List[str]): A list to collect IDs of instances that failed to start.
+        instance_ids (List[str]): A list of EC2 instance IDs to start.
 
     Returns:
         None
     """
+    if not instance_ids:
+        print("[INFO]: No EC2 instances to start.")
+        return
     try:
-        ec2_client.start_instances(InstanceIds=ec2_instanceIds)
-        print(f"[INFO]: Successfully started instance(s): '{ec2_instanceIds}'.")
+        ec2_client.start_instances(InstanceIds=instance_ids)
+        print(f"[INFO]: Successfully started instances: {instance_ids}")
     except Exception as e:
-        raise Exception(f"[ERROR]: Failed to start instance {ec2_instanceIds}: {str(e)}")
+        print(f"[ERROR]: Failed to start instances {instance_ids}: {e}")
+        raise
 
-def handler(event, context):
+def handler(event: Dict, context: Optional[Dict] = None) -> None:
     """
     AWS Lambda handler to start or stop EC2 instances based on the action in the event.
-    """
-    print(f"Lambda triggered with event: {event}")
 
-    # get action and validate if it they are valid actions. 
+    Args:
+        event (Dict): The event data passed to the Lambda function.
+        context (Optional[Dict]): The context object passed to the Lambda function.
+
+    Returns:
+        None
+    """
+    print(f"[INFO]: Lambda triggered with event: {event}")
     action = event.get('action')
     if action not in ["start", "stop"]:
-        raise Exception("[ERROR]: Invalid action provided. Must be 'start' or 'stop'.")
-    
+        print("[ERROR]: Invalid action provided. Must be 'start' or 'stop'.")
+        raise ValueError("Invalid action provided. Must be 'start' or 'stop'.")
+
     try:
+        instance_ids = []
         if action == "start":
             instance_ids = get_ec2_instance_ids_by_schedule_tag(START_TAG_KEY, START_TAG_VALUE)
-            start_ec2_instance(instance_ids)
-            success_message = f"[INFO]: Successfully started EC2 instances: {'\n'.join([])}"
-            log_and_report_process_results(False, success_message)
+            start_ec2_instances(instance_ids)
         elif action == "stop":
             instance_ids = get_ec2_instance_ids_by_schedule_tag(STOP_TAG_KEY, STOP_TAG_VALUE)
-            stop_ec2_instance(instance_ids)
-            success_message = f"[INFO]: Successfully stopped EC2 instances: {'\n'.join([])}"
-            log_and_report_process_results(False, success_message)
+            stop_ec2_instances(instance_ids)
+
+        success_message = f"Successfully {action}ed EC2 instances: {', '.join(instance_ids) if instance_ids else 'None'}"
+        log_and_report_process_results(False, success_message)
     except Exception as e:
-        log_and_report_process_results(True, str(e))    
+        log_and_report_process_results(True, str(e))
+        raise
